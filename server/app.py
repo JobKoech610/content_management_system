090a5d20464b1dd433abe97d514fe4c63cdda0c0
Sharoun Madoya <madoyasharoun@gmail.com>
2024-04-17 11:24:20 +0300

    :beers: created platform route

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from flask import db
+from flask import db, Platform
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -19,3 +19,19 @@ db.init_app(app)
 @app.route('/')
 def index():
     return "content management system"
+
+@app.route('/platforms', methods=['GET'])
+def platforms():
+    if request.method == 'GET':
+        platforms=[]
+        for all platform in Platform.query.all():
+            platform_dict ={
+                "publisher": platform.publisher,
+                "description": platform.description,
+                "image": platform.image
+            }
+        platforms.append(platform_dict)
+        response = make_response(
+            jsonify(platforms), 200
+        )
+        return response
