a1eb583b2ddd73578bc0ad72dc268d5da7b38ba8
JobKoech610 <jobkoech610@gmail.com>
2024-05-07 14:27:23 +0300

    :package: init module

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 db = SQLAlchemy()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://content_management:SHAR0007@localhost/content_management_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://content_management:postgres@localhost:5432/content_management_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = '{~T<B#c&Y@oP}"C}pc7ajYR},Etow+Sc'
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
+jwt= JWTManager(app)
