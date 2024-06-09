1e562a41a79ead8f545941be6251b62b2584b237
JobKoech610 <jobkoech610@gmail.com>
2024-06-09 19:56:21 +0300

    :passport_control: solved the login authorization error by hashing the passoword

---
+++ client/src/components/views/LogIn.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+// import { useNavigate } from 'react-router-dom'; 
+
+function LogIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    // const navigate = useNavigate(); 
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://127.0.0.1:5000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            const data = await response.json();
+            if (data.token) {
+                localStorage.setItem('token', data.token);
+                // navigate('/home');
+                console.log("Welcome")
+            } else {
+                setError(data.message || "Login failed");
+            }
+        } catch (error) {
+            setError('An error occurred, please try again');
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleLogin}>
+                <label>Email</label>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
+                <label>Password</label>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <button type="submit">Login</button>
+            </form>
+            {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display error message */}
+        </div>
+    );
+}
+
+export default LogIn;
